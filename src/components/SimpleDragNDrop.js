2669bb9e79ce89149a17c29842ebe4a345bb2d27
the-dimensionless <sumit.sssingh.singh45@gmail.com>
2020-10-18 22:15:55 +0530

    drag n drop using 2x1D Animation

--- src/components/SimpleDragNDrop.js
+++ src/components/SimpleDragNDrop.js
@@ -1,21 +1,74 @@
 import React from 'react';
-import { View, Text, StyleSheet, Animation } from 'react-native';
+import { View, Text, Animated, PanResponder, StyleSheet } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+
+const USE_NATIVE_DRIVER = true;
 
 const SimpleDragNDrop = () => {
+
+    let dragItem = React.createRef();
+
+    let translateX = new Animated.Value(0);
+    let translateY = new Animated.Value(0);
+    let lastOffset = { x: 0, y: 0 };
+
+    let onPanGestureEvent = Animated.event(
+        [
+            {
+                nativeEvent: {
+                    translationX: translateX,
+                    translationY: translateY,
+                },
+            },
+        ],
+        { useNativeDriver: USE_NATIVE_DRIVER }
+    )
+
+    const onPanStateChangeHandler = (evt) => {
+        if (evt.nativeEvent.oldState === State.ACTIVE) {
+            lastOffset.x += evt.nativeEvent.translationX;
+            lastOffset.y += evt.nativeEvent.translationY;
+            translateX.setOffset(lastOffset.x);
+            translateX.setValue(0);
+            translateY.setOffset(lastOffset.y);
+            translateY.setValue(0);
+        }
+    }
+
+    let panStyle = {
+        transform: [
+            { translateX }, { translateY },
+        ]
+    }
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Hello I am a text!</Text>
-        </View>
+        <PanGestureHandler
+            onGestureEvent={onPanGestureEvent}
+            onHandlerStateChange={onPanStateChangeHandler}
+            ref={dragItem}
+            shouldCancelWhenOutside={true}
+        >
+            <Animated.View style={[panStyle, styles.container]}>
+                <Text style={[
+                    styles.text
+                ]}>
+                    Hello I am text!
+                                </Text>
+            </Animated.View>
+        </PanGestureHandler>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        position: 'absolute',
+        alignItems: 'center',
         justifyContent: 'center',
+        backgroundColor: 'yellow',
+        height: 200
     },
     text: {
-        fontSize: 23,
+        fontSize: 23
     }
 })
 
